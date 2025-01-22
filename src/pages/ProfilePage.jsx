f1f88a08a72765cd6301dd23d3df395094f66169
PhillipSegura <PhillipaSegura@gmail.com>
2025-01-22 06:56:11 -0700

    filled out the code needed for the profile page

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import {useEffect, useState } from 'react';
+import axios from 'axios';
+
 
 function ProfilePage() {
-  return <div>ProfilePage</div>;
+    const [ entries, setEntries ] = useState ([]);
+    const LOCAL_URL = 'http;localhost:5050';
+
+    const getEntries = async () => {
+        console.log('in getEntries');
+    // fetch calendar entries from the backend
+    // also know as the api that i am creating
+    try{
+        const response = await axios.get(`$LOCAL_URL}/api/profile`);
+        console.log(response.data);
+        setEntries(response.data);
+    }catch (err) {
+        console.error(err);
+    }
+
+    }
+  return (
+  <div>ProfilePage</div>;
+  )
 }
 
 export default ProfilePage;
